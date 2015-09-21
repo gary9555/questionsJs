44954d6d8d42ad4d4bad2e2409f27e20117cb28d
gary9555 <gary9555@gmail.com>
2015-09-21 17:55:10 +0800

    second

--- test/unit/controllerTest.js
+++ test/unit/controllerTest.js
@@ -3,7 +3,7 @@
 describe('sorting the list of users', function() {
   it('sorts in descending order by default', function() {
     var users = ['jack', 'igor', 'jeff'];
-    //    var sorted = sortUsers(users);
+     //   var sorted = sortUsers(users);
     //    expect(sorted).toEqual(['jeff', 'jack', 'igor']);
   });
 });
@@ -51,7 +51,7 @@ describe('TodoCtrl', function() {
           {str:"Hello.co? This is Sung", exp: "Hello.co?"},
           {str:"Hello.co This is Sung", exp: "Hello.co This is Sung"},
           {str:"Hello.co \nThis is Sung", exp: "Hello.co \n"},
-
+		  {str:"Hello.co! This! is! Sung", exp: "Hello.co!"},
           {str:"Hello?? This is Sung", exp: "Hello??"},
         ];
 
@@ -71,6 +71,150 @@ describe('TodoCtrl', function() {
 
         expect(scope.roomId).toBe("new");
       });
+	  
+	 
+	  // Watch Collection
+	  it('$watchCollection',function() {
+		var ctrl = controller('TodoCtrl', {
+          $scope: scope
+        });
+	 
+	 
+	  });
+	  
+	  // add todo 
+	/*  
+	  it('addTodo',function() {
+		var ctrl = controller('TodoCtrl', {
+          $scope: scope,
+		  $location: location,
+          $firebaseArray: firebaseArray,
+          $sce: sce,
+          $localStorage: localStorage,
+          $window: window
+        });  
+	 	
+		scope.input.wholeMsg = "shuai";
+		
+	 	scope.addTodo();
+		
+	  });
+	 */
+	  
+	  // edit todo
+	  it('editTodo',function() {
+		var ctrl = controller('TodoCtrl', {
+          $scope: scope
+        });  
+	 	
+		var input="cool";
+		scope.editTodo(input);
+	 	expect(scope.editedTodo).toBe(input);
+	//	expect(scope.originalTodo).toBe(["cool"]);
+	  });
+	  
+	  
+	  // add echo
+	  it('addEcho',function() {
+		var ctrl = controller('TodoCtrl', {
+          $scope: scope
+        });  
+		
+		var td = {
+			wholeMsg: "shuai",
+			head: "shuai",
+			headLastChar: 'i',
+			desc: "hi",
+		//	linkedDesc: Autolinker.link(desc, {newWindow: false, stripPrefix: false}),
+			completed: false,
+			timestamp: new Date().getTime(),
+			tags: "...",
+			echo: 0,
+			order: 0
+		};
+		
+		scope.addEcho(td);
+		
+		
+	 	expect(td.echo).toBe(1);
+		expect(td.order).toBe(-1);
+		expect(scope.$storage[td.$id]).toBe("echoed");
+	  });
+
+	  // done editing
+	  it('doneEditing',function() {
+		var ctrl = controller('TodoCtrl', {
+          $scope: scope
+        });  
+		
+		var td1 = {
+			wholeMsg: "shuai",
+			head: "shuai",
+			headLastChar: 'i',
+			desc: "hi",
+			completed: false,
+			timestamp: new Date().getTime(),
+			tags: "...",
+			echo: 0,
+			order: 0
+		};
+		var td2 = {
+			wholeMsg: "",
+			head: "shuai",
+			headLastChar: 'i',
+			desc: "hi",
+			completed: false,
+			timestamp: new Date().getTime(),
+			tags: "...",
+			echo: 0,
+			order: 0
+		};
+	 	
+		scope.doneEditing(td1);
+		scope.doneEditing(td2);
+		
+	 	//expect(scope.editedTodo).toBe(input);
+	
+	  });
+		
+		// clear completed todos
+	   it('clearCompletedTodos',function() {
+		var ctrl = controller('TodoCtrl', {
+          $scope: scope
+        });  
+		
+		var td1 = {
+			wholeMsg: "shuai",
+			head: "shuai",
+			headLastChar: 'i',
+			desc: "hi",
+			completed: false,
+			timestamp: new Date().getTime(),
+			tags: "...",
+			echo: 0,
+			order: 0
+		};
+		var td2 = {
+			wholeMsg: "",
+			head: "shuai",
+			headLastChar: 'i',
+			desc: "hi",
+			completed: true,
+			timestamp: new Date().getTime(),
+			tags: "...",
+			echo: 0,
+			order: 0
+		};
+		
+		
+	 	scope.clearCompletedTodos();
+		
+		
+	 	//expect(scope.editedTodo).toBe(input);
+	
+	  });
+
+
 
       it('toTop Testing', function() {
 
@@ -89,3 +233,29 @@ describe('TodoCtrl', function() {
       });
     });
   });
+  
+  
+describe('Unit testing directives', function() {
+  var $compile,
+      $rootScope;
+
+  // Load the myApp module, which contains the directive
+  beforeEach(module('todomvc'));
+
+  // Store references to $rootScope and $compile
+  // so they are available to all tests in this describe block
+  beforeEach(inject(function(_$compile_, _$rootScope_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+  
+  it('Replaces the element with the appropriate content', function() {
+    // Compile a piece of HTML containing the directive
+    var element = $compile("<a-great-eye></a-great-eye>")($rootScope);
+    // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
+    $rootScope.$digest();
+    // Check that the compiled element contains the templated content
+    expect(element.html()).toContain("lidless, wreathed in flame, 2 times");
+  });
+});
--- test/unit/dummyTest.js
+++ test/unit/dummyTest.js
@@ -1,3 +1,12 @@
+describe('todomvc testing', function() {
+  beforeEach(module('todomvc'));
+
+  describe('app function testing', function() {
+    it('current version', inject(function(){}));
+  });
+});
+
+
 describe('Service unit testing', function() {
   beforeEach(module('myApp.services'));
 
@@ -20,6 +29,12 @@ describe('controllers unit test', function(){
     });
     expect(scope.test1).toBe('EFG');
   }));
+  
+  /*controller('MyCtrl1', ['$scope', function($scope) {
+  		$scope.test1 = 'EFG';
+	}])*/
+  
+  
   it('MyCtrl2 controller test2', inject(function($rootScope, $controller) {
     scope = $rootScope.$new();
     var ctrl = $controller('MyCtrl2', {
@@ -33,8 +48,21 @@ describe('controllers unit test', function(){
     var ctrl = $controller('MyCtrl1', {
       $scope : scope
     });
-    var results = scope.getFirstAndRestSentence("Hello? This is Sung");
-    expect(results[0]).toEqual('Hello?');
+  //  var results = scope.getFirstAndRestSentence("Hello? This is Sung");
+   // expect(results[0]).toEqual('Hello?');
+//	expect(results[1]).toEqual(' This is Sung');
+	
+	var res1 = scope.getFirstAndRestSentence("My name is Gary. I'm a student");
+	expect(res1[0]).toEqual('My name is Gary.');
+	
+	var res2 = scope.getFirstAndRestSentence("My name is Gary");
+	expect(res2[0]).toEqual('My name is Gary');
+	
+	var res3 = scope.getFirstAndRestSentence("My name is Gary?I'm a student. I love soccer");
+	expect(res3[0]).toEqual('My name is Gary?');
+	
+	
+
   }));
 
 });
